f5564b56165830900a0f293de785b34ca04216ef
Karol Pietruczuk <karol.pietruczuk@gmail.com>
2022-07-23 19:50:42 +0200

    Added general style user-select & little bit code formatted.

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -2,10 +2,10 @@ import logo from '../../img/logo.webp';
 import './Logo.css';
 
 interface Props {
-    width?: string;
-    margin?: string;
+  width?: string;
+  margin?: string;
 }
 
 export const Logo = ({width = '100px', margin = '0 auto 30px'}: Props) => {
-    return <img src={logo} alt='Logo bootcampu MegaK' style={{width: width, margin: margin}}/>;
+  return <img src={logo} alt='Logo bootcampu MegaK' style={{width: width, margin: margin}}/>;
 };
