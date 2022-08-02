50d0cafefb61cf1f46d1fddfd2e93c75946419ac
Karol Pietruczuk <karol.pietruczuk@gmail.com>
2022-08-02 15:30:39 +0200

    Added Routing.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import {Navigate, Route, Routes} from 'react-router-dom';
 import {Students} from './components/layouts/Students/Students';
+import {Login} from './components/layouts/Login/Login';
 
 export const App = () => {
     return (
-        <>
-            <Students/>
-        </>
+        <Routes>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/students' element={<Students/>}/>
+            <Route path='/' element={<Navigate to='/students'/>}/>
+        </Routes>
     );
 };
