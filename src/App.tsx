826566ddd8fa7732ada3b984e29bb4666be1603e
Karol Pietruczuk <karol.pietruczuk@gmail.com>
2022-08-02 15:45:45 +0200

    Init commit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {NavBar} from './components/common/NavBar/NavBar';
-import {Login} from './components/layouts/login/Login';
+import {Login} from './components/layouts/Login/Login';
 
 export const App = () => {
     return (
