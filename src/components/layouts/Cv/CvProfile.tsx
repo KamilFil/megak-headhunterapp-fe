4631de801200e9baf27c13d14a8b7a130bbb1fcb
Aleksander <azurawski@outlook.com>
2022-08-08 21:53:51 +0200

    Handled avatar display in CvProfile component.

--- src/components/layouts/Cv/CvProfile.tsx
+++ src/components/layouts/Cv/CvProfile.tsx
@@ -9,40 +9,47 @@ interface Props {
 export const CvProfile = (props: Props) => {
   return (
     <div className='coursant-profile'>
-        <div key={props.data.email} className='coursant-profile_item'>
-          <img src='/img/logo-test.png' alt='ALT' />
-          <p className='coursant_profile-item_title'>
-            {props.data.firstName} {props.data.lastName}
-          </p>
-          <div className='coursant_profile-item_github'>
-            <i className='fa-brands fa-github'></i>
-            <a
-              className='coursant_profile-item_github-link'
-              href={`https://github.com/${props.data.githubUsername}`}>
-              {props.data.githubUsername}
-            </a>
+      <div key={props.data.email} className='coursant-profile_item'>
+        <img
+          src={
+            !props.data.githubUsername
+              ? '/img/logo-test.png'
+              : `https://github.com/${props.data.githubUsername}.png`
+          }
+          alt='avatar'
+        />
+        <p className='coursant_profile-item_title'>
+          {props.data.firstName} {props.data.lastName}
+        </p>
+        <div className='coursant_profile-item_github'>
+          <i className='fa-brands fa-github'></i>
+          <a
+            className='coursant_profile-item_github-link'
+            href={`https://github.com/${props.data.githubUsername}`}>
+            {props.data.githubUsername}
+          </a>
+        </div>
+        <div className='coursant-profile_item-contact'>
+          <div className='coursant-profile_item-contact_mail'>
+            <i className='fa-solid fa-phone'></i>
+            <a href={`tel:${props.data.tel}`}>+48 {props.data.tel}</a>
           </div>
-          <div className='coursant-profile_item-contact'>
-            <div className='coursant-profile_item-contact_mail'>
-              <i className='fa-solid fa-phone'></i>
-              <a href={`tel:${props.data.tel}`}>+48 {props.data.tel}</a>
-            </div>
-            <div className='coursant-profile_item-contact_phone'>
-              <i className='fa-solid fa-envelope'></i>
-              <a href={`mailto:${props.data.email}`}>{props.data.email}</a>
-            </div>
-          </div>
-          <div className='coursant-profile_item-bio'>
-            <p className='coursant-profile-item_bio-title'>O mnie:</p>
-            <p>{props.data.bio}</p>
-          </div>
-          <div>
-            <p className='coursant-profile_item-action'>Brak zainteresowania</p>
-          </div>
-          <div>
-            <p className='coursant-profile_item-action'>Zatrudniony</p>
+          <div className='coursant-profile_item-contact_phone'>
+            <i className='fa-solid fa-envelope'></i>
+            <a href={`mailto:${props.data.email}`}>{props.data.email}</a>
           </div>
         </div>
+        <div className='coursant-profile_item-bio'>
+          <p className='coursant-profile-item_bio-title'>O mnie:</p>
+          <p>{props.data.bio}</p>
+        </div>
+        <div>
+          <p className='coursant-profile_item-action'>Brak zainteresowania</p>
+        </div>
+        <div>
+          <p className='coursant-profile_item-action'>Zatrudniony</p>
+        </div>
+      </div>
     </div>
   );
 };
