074309dcc6819eb24f7196c317d81d0533a63e64
Kamil F <kamilfprog@gmail.com>
2022-08-09 21:31:50 +0200

    - Fix problem type for BE

--- src/components/layouts/Cv/Cv.tsx
+++ src/components/layouts/Cv/Cv.tsx
@@ -8,38 +8,21 @@ import { getStudentUser } from '../../../api/api';
 import { useParams } from 'react-router-dom';
 
 export const Cv = () => {
-  const student: StudentEntity = {
-    id: '',
-    email: '',
-    tel: '',
-    firstName: '',
-    lastName: '',
-    githubUsername: '',
-    portfolioUrls: [],
-    projectUrls: [],
-    bio: '',
-    expectedTypeWork: '',
-    targetWorkCity: '',
-    expectedContractType: '',
-    expectedSalary: 0,
-    canTakeApprenticeship: true,
-    monthsOfCommercialExp: 0,
-    education: '',
-    workExperience: '',
-    courses: '',
-    isActive: true,
-    hireStatus: '',
-  };
-  const [studentUser, setStudentUser] = useState(student);
+  const [studentUser, setStudentUser] = useState<StudentEntity | null>(null);
   const { id } = useParams();
 
   useEffect(() => {
     (async () => {
       const res = await getStudentUser(id as string);
-
+      console.log(res);
+      // const data = await res.json();
       setStudentUser(res.data);
     })();
   }, []);
+
+  if (studentUser === null) {
+    return <p>Ładowanie zasobów</p>;
+  }
 
   return (
     <>
