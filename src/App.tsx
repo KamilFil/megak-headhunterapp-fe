f4194d63b7306bcabd9607b03dc9d7edd90ec019
Kamil F <kamilfprog@gmail.com>
2022-08-10 21:42:02 +0200

    - Work to connect FE/BE login system

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { Students } from './components/layouts/Students/Students';
 import { Login } from './components/layouts/Login/Login';
 import { Cv } from './components/layouts/Cv/Cv';
--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,31 +1,66 @@
-import React, {FormEvent, useState} from 'react';
-import {LoginInput} from '../LoginInput/LoginInput';
-import {RedButton} from '../common/RedButton/RedButton';
+import React, { FormEvent, useEffect, useState } from 'react';
+import { LoginInput } from '../LoginInput/LoginInput';
+import { Route, Router, useNavigate } from 'react-router-dom';
+
+import { RedButton } from '../common/RedButton/RedButton';
 import './LoginForm.css';
 
 export const LoginForm = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [pwd, setPwd] = useState('');
 
-    const handleLogin = (e: FormEvent) => {
-        e.preventDefault();
-    };
+  const [errMsg, setErrMsg] = useState('');
+  const [success, setSuccess] = useState(false);
 
-    const handleClick = () => {
-        console.log('CLICK');
-    };
+  const navigate = useNavigate();
+  useEffect(() => {
+    setErrMsg('');
+  }, [email, pwd]);
 
-    return (
-        <form onSubmit={handleLogin}>
-            <LoginInput type='email' placeholder='E-mail' value={email} setValue={setEmail}/>
-            <LoginInput type='password' placeholder='Hasło' value={password} setValue={setPassword}/>
-            <a className='forgotten-password'>Zapomniałeś hasła?</a>
-            <div className='submit-wrap'>
-                <p>
-                    <span>Nie masz konta?</span> <a>Zarejestruj się</a>
-                </p>
-                <RedButton name='Zaloguj się' type='submit' handleClick={handleClick}/>
-            </div>
+  const handleClick = async (e: FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch('http://localhost:3001/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, pwd }),
+      });
+      const data = await res.json();
+
+      if (data.roles === 'hr') {
+        navigate('/students');
+      } else if (data.roles !== 'hr') {
+        navigate('/brak');
+      }
+
+      console.log(data);
+    } catch (e) {
+      console.error(e);
+    }
+    setSuccess(true);
+  };
+
+  return (
+    <>
+      {success ? (
+        <></>
+      ) : (
+        <form>
+          <LoginInput type='email' placeholder='E-mail' value={email} setValue={setEmail} />
+          <LoginInput type='password' placeholder='Hasło' value={pwd} setValue={setPwd} />
+          <a className='forgotten-password'>Zapomniałeś hasła?</a>
+          <div className='submit-wrap'>
+            <p>
+              <span>Nie masz konta?</span> <a>Zarejestruj się</a>
+            </p>
+            {/* <RedButton name='Zaloguj się' type='submit' handleClick={handleClick} />*/}
+            <button type='submit' onClick={handleClick}>
+              Zaloguj dsie
+            </button>
+          </div>
         </form>
-    );
+      )}
+    </>
+  );
 };
