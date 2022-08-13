e3f6f4693c104547e69722c35cdb30630b9a378d
Aleksander <azurawski@outlook.com>
2022-08-14 01:06:28 +0200

    Created structure in ImportButton component.

--- src/components/ImportButton/ImportButton.tsx
+++ src/components/ImportButton/ImportButton.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { ChangeEvent, SyntheticEvent, useState } from 'react';
+import { uploadUsersList } from '../../api/api';
 
 export const ImportButton = () => {
+  const [selectedFile, setSelectedFile] = useState('');
+
+  const submit = () => {
+    const data = new FormData();
+    data.append('list', selectedFile);
+
+    console.log(data);
+
+    uploadUsersList(data);
+  };
+
+  console.log(selectedFile);
+
   return (
-    <div>
-      <input type='file' />
+    <div className='import-form'>
+      <div className='import-form--upload-file'>
+        <label>Wybierz plik:</label>
+        <input
+          type='file'
+          className='upload-file'
+          value={selectedFile}
+          // onChange={(e) => setSelectedFile({ selectedFile: e.target.files[0] })}
+        />
+      </div>
+      <div className='import-form--submit-button'>
+        <button type='submit' onClick={submit}>
+          Zapisz
+        </button>
+      </div>
     </div>
   );
 };
