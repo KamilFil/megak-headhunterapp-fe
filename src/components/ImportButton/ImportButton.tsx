a20c1bd121d93ab31ac1388f1a51785ff537ab24
Aleksander <azurawski@outlook.com>
2022-08-14 02:02:11 +0200

    Connected ImportButton component with BE.

--- src/components/ImportButton/ImportButton.tsx
+++ src/components/ImportButton/ImportButton.tsx
@@ -1,36 +1,35 @@
 import React, { ChangeEvent, SyntheticEvent, useState } from 'react';
 import { uploadUsersList } from '../../api/api';
 
+import './ImportButton.css';
+
 export const ImportButton = () => {
   const [selectedFile, setSelectedFile] = useState('');
 
-  const submit = () => {
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
     const data = new FormData();
     data.append('list', selectedFile);
 
     console.log(data);
+    try {
+      uploadUsersList(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    uploadUsersList(data);
+  const handleSelectedFile = (event: any) => {
+    setSelectedFile(event.target.files[0]);
   };
 
   console.log(selectedFile);
 
   return (
-    <div className='import-form'>
-      <div className='import-form--upload-file'>
-        <label>Wybierz plik:</label>
-        <input
-          type='file'
-          className='upload-file'
-          value={selectedFile}
-          // onChange={(e) => setSelectedFile({ selectedFile: e.target.files[0] })}
-        />
-      </div>
-      <div className='import-form--submit-button'>
-        <button type='submit' onClick={submit}>
-          Zapisz
-        </button>
-      </div>
-    </div>
+    <form onSubmit={handleSubmit} className='upload-form'>
+      <label className='upload-form--label'>Import kursantów:</label>
+      <input type='file' className='upload-form--button' onChange={handleSelectedFile} />
+      <input type='submit' className='upload-form--submit' value='Zapisz' />
+    </form>
   );
 };
